405e339200b2255aeb8e01efee6e71a873716cba
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-23 17:21:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
   while array[counter] do
-    new_array.push(array[counter].to_i ** )
+    new_array.push(array[counter].to_i ** 2 )
     counter +=1 
   end 
   new_array
